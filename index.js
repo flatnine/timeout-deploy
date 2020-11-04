91e906b6030c49c641d52fc37f847e2c1bcfe578
Jonathan Burman <jonathan.burman@timeout.com>
2020-11-04 11:50:57 +0000

    add output

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ async function action() {
     const github = require("@actions/github")
     const octokit = github.getOctokit(process.env.GITHUB_TOKEN);
 
-    const app_name = core.getInput(`app_name`);
-    if (!app_name) {
+    const appName = core.getInput(`appName`);
+    if (!appName) {
         console.log("App Name is required");
         return; 
     }
@@ -16,15 +16,15 @@ async function action() {
         return; 
     }
     
-    const domainname = core.getInput(`domainname`);
+    const domainName = core.getInput(`domainName`);
     const imageTag = core.getInput(`imageTag`);
     const graffitiSecret = core.getInput(`graffitiSecret`);
     const accountId = core.getInput(`accountId`);
 
     if (action === 'install') {
         
-        if (!domainname) {
-            console.log("domainname is required");
+        if (!domainName) {
+            console.log("domainName is required");
             return; 
         }
 
@@ -45,10 +45,10 @@ async function action() {
     }
     
     // hostname that will resolve to deployment 
-    const hostname = `www-${github.context.issue.number}.${domainname}`;
+    const hostname = `www-${github.context.issue.number}.${domainName}`;
     const namespace = `pr-${github.context.issue.number}`;   
     // Location of helm chart
-    const chart_path = `charts/${app_name}`;
+    const chart_path = `charts/${appName}`;
 
     let cmdOutput = '';
     let cmdError = '';
@@ -70,7 +70,7 @@ async function action() {
             const GITHUB_WORKSPACE = process.env.GITHUB_WORKSPACE;
             const helm_args = [
                 'upgrade', 
-                app_name,
+                appName,
                 '--install', 
                 '--wait', 
                 '--create-namespace', 
@@ -91,50 +91,52 @@ async function action() {
                 `image.repository=${accountId}.dkr.ecr.eu-west-1.amazonaws.com/gp-web`
             ] 
 
-            let result = await exec.exec('helm', helm_args, options);
-            console.log(`exec.exec('helm', ${helm_args}, ${options})`)
-            console.log(`response ${result}`)
+            await exec.exec('helm', helm_args, options);
             console.log(`Install  Deployment `)
            
         } catch(err) {
             console.log(`output ${cmdOutput}`)
-            const message = `Failed to create deployment ${cmdError}`;
+            core.setOutput("output", cmdError);
             await octokit.issues.createComment({
                 ...github.context.repo,
                 issue_number: github.context.issue.number,
-                body: message,
+                body: `Failed to create deployment ${cmdError}`,
             });
             return;
         }
-        const message = `Success: http://${hostname}`;
+        
+        core.setOutput("output", cmdOutput);
         await octokit.issues.createComment({
             ...github.context.repo,
             issue_number: github.context.issue.number,
-            body: message,
+            body: `Success: http://${hostname}`,
         });
 
     } else if (action === 'delete') {
         try {
-            let result = await exec.exec('helm', ['status', app_name, '-n', namespace], options);
-            console.log(`response ${result}`)
+            await exec.exec('helm', ['status', appName, '-n', namespace], options);
             console.log(`Deployment exists`)
             try {
-                let result = await exec.exec('helm', ['delete', app_name, '-n', namespace], options);
-                console.log(`response ${result}`)
+                await exec.exec('helm', ['delete', appName, '-n', namespace], options);
                 console.log(`Delete Deployment `)
             } catch(err) {
-                console.log(`output ${cmdOutput}`)
+                console.log(`output ${cmdError}`)
+                core.setOutput("output", cmdError);
                 console.log(`Failed to delete deployment`)
+                return
             }
         } catch(err) {
-            console.log(`output ${cmdErrorOutput}`)
+            console.log(`output ${cmdError}`)
+            core.setOutput("output", cmdError);
             console.log(`No stack present`)
-        }
-        finally {
             return 
         }
+        core.setOutput("output", cmdOutput);
+        return 
+        
     } else {
         console.log(`${action} not implemented`)
+        core.setOutput("output", `${action} not implemented`);
         return
     }
 }
